430684ea63aa27812c94018bfe510545cea4b1ea
Omar Maskati <70995663+omaskati@users.noreply.github.com>
2023-11-16 20:53:36 -0500

    Add game fxn and modify playRound return values

--- scripts.js
+++ scripts.js
@@ -8,45 +8,71 @@ function getComputerChoice(){
 function playRound(playerSelection, computerSelection) {
 
     if(playerSelection.toLowerCase() === computerSelection.toLowerCase()){
-        return "It's a tie!";
+        return [0, "It's a tie!"];
     }
 
     else if(playerSelection.toLowerCase() === "rock"){
         if(computerSelection.toLowerCase() === "paper"){
-            return "You lose! Paper beats rock.";
+            return [-1, "You lose! Paper beats rock."];
         }
         else if(computerSelection.toLowerCase() === "scissors"){
-            return "You win! Rock beats scissors.";
+            return [1, "You win! Rock beats scissors."];
         }
         else return `${computerSelection} is not a valid input`;
     }
 
     else if(playerSelection.toLowerCase() === "paper"){
         if(computerSelection.toLowerCase() === "rock"){
-            return "You win! Paper beats rock.";
+            return [1, "You win! Paper beats rock."];
         }
         else if(computerSelection.toLowerCase() === "scissors"){
-            return "You lose! Scissors beats paper.";
+            return [-1, "You lose! Scissors beats paper."];
         }
         else return `${computerSelection} is not a valid input`;
     }
 
     else if(playerSelection.toLowerCase() === "scissors"){
         if(computerSelection.toLowerCase() === "rock"){
-            return "You lose! Rock beats scissors.";
+            return [-1, "You lose! Rock beats scissors."];
         }
         else if(computerSelection.toLowerCase() === "paper"){
-            return "You win! Scissors beats paper.";
+            return [1, "You win! Scissors beats paper."];
         }
         else return `${computerSelection} is not a valid input`;
     }
 
     else{
-        return "Your input was not 'rock', 'paper', or 'scissors'. Try again!";
+        return [undefined, "Your input was not 'rock', 'paper', or 'scissors'. Try again!"];
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(`Computer played ${computerSelection}`);
-console.log(playRound(playerSelection, computerSelection));
+function game(){
+    let numPlayerWins = 0;
+    let numCompWins = 0;
+    let numTies = 0;
+    for (let i=1; i<6; i++){
+        let playerSelection = prompt("Choose 'rock', 'paper', or 'scissors'!");
+        while(playerSelection.toLowerCase() !== "rock" && playerSelection.toLowerCase() !== "paper" && playerSelection.toLowerCase() !== "scissors"){
+            playerSelection = prompt("Try again. Choose 'rock', 'paper', or 'scissors'!")
+        }
+        let computerSelection = getComputerChoice();
+        console.log(`Computer played ${computerSelection}`);
+        let result = playRound(playerSelection, computerSelection);
+        console.log(result[1]);
+        if(result[0] === 1){
+            numPlayerWins++;
+        }
+        else if(result[0] === 0){
+            numTies++;
+        }
+        else if(result[0] === -1){
+            numCompWins++;
+        }
+        else return;
+
+        console.log(`Your record after Round ${i}: ${numPlayerWins}-${numCompWins}-${numTies}`);
+    }
+
+    console.log(numPlayerWins>numCompWins?"You win!":(numPlayerWins<numCompWins?"You lose :(": "Draw."));
+    return true;
+}
